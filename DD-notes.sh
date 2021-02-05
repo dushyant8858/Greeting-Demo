b29c45614f1bf7775614285b5563edc9fbcec51a
Dushyant <dushyant.dubaria2010@gmail.com>
2021-02-05 12:23:22 -0600

    Added test line in Dockerfile

--- DD-notes.sh
+++ DD-notes.sh
@@ -3,8 +3,12 @@
 docker build -t greeting:latest --build-arg server_port=8084 --no-cache .
 docker stop greeting && docker rm greeting 
 docker run -d -p 8084:8084 --name greeting greeting
-curl localhost:8084/greeting
+curl localhost:8084/greeting | jq .
 
+
+docker logs greeting
+docker exec -it greeting sh 
+apk add curl 
 
 
 git add .
